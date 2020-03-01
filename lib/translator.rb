4286ea9d0906d0cd1d0fc37562eb883b015b2201
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-03-01 17:13:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,18 @@
 require 'yaml'
+require 'pp'
+def load_library(path)
+  #emoticons = YAML.load_file(path)
+  emoticon_hash = Hash.new
 
+  emoticon_hash["get_emoticon"] = Hash.new
+  emoticon_hash["get_meaning"] = Hash.new
+
+  emoticons.each do |english_word, emoticon_set|
+    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
+    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
+  end
+  pp emoticon_hash
+end
 # def load_library(path)
 #   emoticons_meaning = {"get_meaning" => {}, "get_emoticon" => {}}
 #   emoticons.each do |meaning_key, emoticon_value|
