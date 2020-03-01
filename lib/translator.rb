38bd5e61973dffbe5f62a1e95c2fecf8ecc54c41
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-03-01 17:13:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,19 +1,19 @@
 require 'yaml'
 
-def load_library(path)
-  emoticons_meaning = {"get_meaning" => {}, "get_emoticon" => {}}
-  emoticons.each do |meaning_key, emoticon_value|
-    emoticons_meaning["get_meaning"][emoticon_value] = meaning_key
-  end
-end
+# def load_library(path)
+#   emoticons_meaning = {"get_meaning" => {}, "get_emoticon" => {}}
+#   emoticons.each do |meaning_key, emoticon_value|
+#     emoticons_meaning["get_meaning"][emoticon_value] = meaning_key
+#   end
+# end
 
-def get_japanese_emoticon(path, emoticons)
-  # code goes here
-end
+# def get_japanese_emoticon(path, emoticons)
+#   # code goes here
+# end
 
-def get_english_meaning(path, emoticons)
-  # code goes here
-end
+# def get_english_meaning(path, emoticons)
+#   # code goes here
+# end
 
 
 
