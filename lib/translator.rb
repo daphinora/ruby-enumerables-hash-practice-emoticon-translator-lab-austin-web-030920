01895956161c40fb8a20ba72866757d42b67f086
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-03-01 17:22:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ end
 def get_japanese_emoticon(path, emoticon)
   emoticons_meaning = load_library(path)
   translator = emoticons_meaning["get_meaning"][emoticon]
-  if translator === nil
+  if translator == nil
     return "Sorry, that emoticon was not found"
   end
   translator
