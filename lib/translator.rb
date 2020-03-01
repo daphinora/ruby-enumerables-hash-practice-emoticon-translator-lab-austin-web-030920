2ce77a3786364723bb30bb19d30e536032d076ca
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-03-01 17:16:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,11 @@
 require 'yaml'
 require 'pp'
 def load_library(path)
+  emoticons = 
   emoticons_meaning = {"get_meaning" => {}, "get_emoticon" => {}}
   emoticons.each do |meaning_key, emoticon_value|
     emoticons_meaning["get_meaning"][emoticon_value] = meaning_key
+    
   end
   pp emoticons_meaning
 end
