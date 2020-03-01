4b425c9228cc513ca7b8127d0f3971ebdbb9357c
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-03-01 17:25:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,12 +2,14 @@ require 'yaml'
 require 'pp'
 def load_library(path)
   emoticons = YAML.load_file(path)
+#calls on the yaml path properly
   emoticons_meaning = {"get_meaning" => {}, "get_emoticon" => {}}
   emoticons.each do |meaning, emoticon|
     emoticons_meaning["get_emoticon"][emoticon.first] = emoticon.last
     emoticons_meaning["get_meaning"][emoticon.last] = meaning
   end
   pp emoticons_meaning
+  #pretty print so it returns the actual symbols instead of gibberish
 end
 
 
@@ -22,6 +24,7 @@ end
 
 
 def get_english_meaning(path, emoticon)
+  #same thing as get_japanese_emoticon except with "get_meaning" instead of "get_emoticon"--instead of giving A and getting B, we're giving B and expecting A (if they exist)
   emoticons_meaning = load_library(path)
   translator = emoticons_meaning["get_meaning"][emoticon]
   if translator == nil
